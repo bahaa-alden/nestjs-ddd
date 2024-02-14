789a8a0fee1f3e814623562f6f473a3f22591762
bahaa-alden <samerabood195@gmail.com>
2024-02-14 20:49:53 +0300

    first commit

---
+++ src/database/seeds/relational/seed.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import appConfig from 'src/config/app.config';
+import databaseConfig from 'src/database/config/database.config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmConfigService } from '../../typeorm-config.service';
+import { RoleSeedModule } from './role/role-seed.module';
+import { StatusSeedModule } from './status/status-seed.module';
+import { UserSeedModule } from './user/user-seed.module';
+
+// import { unnamedSeedModule } from './unnamed/unnamed-seed.module';
+
+@Module({
+  imports: [
+    // unnamedSeedModule,
+    RoleSeedModule,
+    StatusSeedModule,
+    UserSeedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+  ],
+})
+export class SeedModule {}
