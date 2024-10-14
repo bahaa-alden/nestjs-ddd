01578a41f8844e7875828cf166f441140f784d8a
bahaa-alden <samerabood195@gmail.com>
2024-10-14 12:07:39 +0300

    fix: add logger

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,34 @@ import {
   VersioningType,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
 import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
+import { LoggerService } from './logger/logger.service';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  const httpAdapter = app.get(HttpAdapterHost);
+  const appEnv = configService.getOrThrow('app.nodeEnv', { infer: true });
+  const logger = app.get(LoggerService);
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapter, appEnv));
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   app.enableShutdownHooks();
   app.setGlobalPrefix(
@@ -47,5 +63,10 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
+
+  logger.log(
+    'NestApplication',
+    `Docs started on: ${configService.getOrThrow('app.backendDomain', { infer: true })}/docs`,
+  );
 }
 void bootstrap();
